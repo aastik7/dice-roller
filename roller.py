76c002fc0e461871b7aca47c8e6acdff3a35d0cd
Aastik <aastikdude7@gmail.com>
2024-02-03 23:28:33 +0530

    Print Horizontally

--- roller.py
+++ roller.py
@@ -42,9 +42,17 @@ num_of_dice = int(input("How many dice?: "))
 for die in range(num_of_dice):
     dice.append(random.randint(1, 6))
 
-#PRINT VERTICALLY
-for die in range(num_of_dice):
-   for line in dice_art.get(dice[die]):
-       print(line)
+# PRINT VERTICALLY
+# for die in range(num_of_dice):
+#    for line in dice_art.get(dice[die]):
+#        print(line)
 
-PRINT HORIZONTALLY
+# PRINT HORIZONTALLY
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line], end="")
+    print()
+
+for die in dice:
+    total += die
+print(f"total: {total}")
